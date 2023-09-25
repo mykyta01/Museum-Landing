6a093cae15f7721151899e722dd80e33b01a1327
Nick Romanishyn <126708945+mykyta01@users.noreply.github.com>
2023-09-25 17:07:47 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -50,47 +50,62 @@ const swiper = new Swiper('.gallery__swiper', {
   speed: 400,
   spaceBetween: 16,
   loop: true,
-  loopAdditionalSlides: 2,
+  loopAdditionalSlides: 2, // Add additional slides to loop to avoid duplicate slides
 
+  // Configuration for the pagination component of the Swiper
   pagination: {
     el: '.swiper__pagination',
     clickable: true,
   },
 
+  // Configuration for the navigation arrows of the Swiper
   navigation: {
-    prevEl: '.swiper-button-prev',
-    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev', //
+    nextEl: '.swiper-button-next', //
   },
 
+  // Configuration for responsive design breakpoints
   breakpoints: {
-    768: {
+    768: { // When the viewport width is 768 pixels or more
       slidesPerView: 2,
       spaceBetween: 20,
     },
   },
 });
 
+// Define a function to toggle the Swiper functionality based on window width
 const toggleSwiper = () => {
   const gallerySlides = document.querySelectorAll('.swiper__slide');
+  
   const currentWidth = window.innerWidth;
+  
+  // Set the width threshold to determine when to toggle Swiper
   const targetWidth = 1280;
 
+  // If the current window width is greater than or equal to the target width
   if (currentWidth >= targetWidth) {
-    swiper.disable();
+    swiper.disable(); // Disable the Swiper
 
+    // Add a class to each slide for desktop styling
     gallerySlides.forEach(slide => {
       slide.classList.add('swiper__slide--desktop');
     });
   } else {
+    // If the window width is less than the target width, remove the desktop-specific class
     gallerySlides.forEach(slide => {
       slide.classList.remove('swiper__slide--desktop');
     });
 
-    swiper.enable();
+    swiper.enable(); // Enable the Swiper
   }
 };
 
+// Attach the toggleSwiper function to the resize event of the window
+// This ensures that the function runs every time the window size changes
 window.addEventListener('resize', toggleSwiper);
+
+// Attach the toggleSwiper function to the load event of the window
+// This ensures that the function runs when the page is first loaded
 window.addEventListener('load', toggleSwiper);
 
 /* =========BUTTON TO TOP============== */
